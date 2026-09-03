93e7bdd3f9832dfd6b9081b650fe2e92a41f7085
jordanlegare <jordanlegare3@gmail.com>
2026-09-03 15:50:50 -0400

    feat: export audio checksum framing

--- ald_media_controller.py
+++ ald_media_controller.py
@@ -15,14 +15,23 @@ import ald_media_codecs as _media
 
 for _name in (
     "FrameDecodeError",
+    "AudioDecodeError",
     "MediaProfile",
     "DEFAULT_MEDIA_PROFILE",
     "DecodedFrameRecord",
+    "AudioRecord",
     "QR_MAGIC",
+    "AUDIO_PREAMBLE",
+    "AUDIO_VERSION",
+    "AUDIO_RECORD_BYTES",
     "encode_qr_payload",
     "decode_qr_payload",
     "render_instruction_frame",
     "decode_instruction_frame",
+    "build_audio_record",
+    "parse_audio_record",
+    "manchester_encode",
+    "manchester_decode",
 ):
     setattr(_core, _name, getattr(_media, _name))
 
