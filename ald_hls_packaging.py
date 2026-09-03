660641a4ef482e720969a746547c367adcbc417d
jordanlegare <jordanlegare3@gmail.com>
2026-09-03 16:16:08 -0400

    fix: account for AAC priming in packet fMP4

--- ald_hls_packaging.py
+++ ald_hls_packaging.py
@@ -154,7 +154,7 @@ def mux_packet_mp4(
             "-ac",
             "1",
             "-movflags",
-            "+frag_keyframe+empty_moov+default_base_moof",
+            "+frag_keyframe+delay_moov+default_base_moof",
             "-f",
             "mp4",
             str(target),
